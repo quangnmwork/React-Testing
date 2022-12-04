06b37cbcfd8cd03dc8422312dc67b9e7e727f790
Quang Nguyen <quangnm.work@gmail.com>
2022-12-04 22:56:22 +0700

    feat: add component testing

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Stepper({
+  initial = 0,
+  onChange = (value: number) => {},
+}) {
+  const [count, setCount] = useState(initial);
+
+  const handleIncrement = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+    onChange(newCount);
+  };
+
+  const handleDecrement = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+    onChange(newCount);
+  };
+
+  return (
+    <div>
+      <button data-cy="decrement" onClick={handleDecrement}>
+        -
+      </button>
+      <span data-cy="counter">{count}</span>
+      <button data-cy="increment" onClick={handleIncrement}>
+        +
+      </button>
+    </div>
+  );
+}
---
+++ src/cypress/support/Commands.ts
@@ -0,0 +1,8 @@
+import { mount } from "cypress/react18";
+import { ReactNode } from "react";
+
+Cypress.Commands.add("mount", (component: ReactNode, options) => {
+  // Wrap any parent components needed
+  // ie: return mount(<MyProvider>{component}</MyProvider>, options)
+  return mount(component, options);
+});
