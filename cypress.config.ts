a3b7de372e5270df1f3dfa77cfadaa9b2ff99c08
Quang Nguyen <quangnm.work@gmail.com>
2022-12-05 12:56:47 +0700

    feat: add record

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: 'g8k44a',
   component: {
     devServer: {
-      framework: "react",
-      bundler: "vite",
+      framework: 'react',
+      bundler: 'vite',
     },
   },
 });
